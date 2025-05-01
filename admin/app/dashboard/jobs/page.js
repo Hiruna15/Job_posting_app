2e32a21ff2154cd75047b21c6fa4beae01815277
Hiruna15 <hirunadilmith15@gmail.com>
2025-05-02 05:25:47 +0530

    load the all job applications inside jobs route

--- admin/app/dashboard/jobs/page.js
+++ admin/app/dashboard/jobs/page.js
@@ -1,7 +1,23 @@
+import JobCard from "@/app/_components/JobCard";
+import { getJobs } from "@/app/_lib/data-service";
+import Link from "next/link";
+
 export const metadata = {
   title: "Jobs",
 };
 
-export default function Page() {
-  return <div></div>;
+export default async function Page() {
+  const jobs = await getJobs();
+
+  return (
+    <div>
+      <div>
+        {jobs.length === 0 ? (
+          <Link href="/dashboard/jobs/new">Create a new Job application</Link>
+        ) : (
+          jobs.map((job) => <JobCard job={job} key={job._id} />)
+        )}
+      </div>
+    </div>
+  );
 }
